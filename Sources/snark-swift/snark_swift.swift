d541a1aba822d3f61a76429b05813270bfc35e26
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2023-11-09 18:57:29 +0800

    feat: add readSection

--- Sources/snark-swift/snark_swift.swift
+++ Sources/snark-swift/snark_swift.swift
@@ -23,6 +23,20 @@ func readBytes(data: [UInt8], startPos: Int, length: Int) -> [UInt8] {
   return bytes
 }
 
+func readSection(data: [UInt8], sections: [[(p: Int, size: UInt64)]], idSection: Int) throws
+  -> [UInt8]
+{
+  let offset = 0
+  let length = Int(sections[idSection][0].size) - offset
+  if (offset + length) > sections[idSection][0].size {
+    throw NSError(domain: "Reading out of the range of the section", code: 0, userInfo: nil)
+  }
+
+  let pointer = sections[idSection][0].p
+  let buff = Array(data[pointer...pointer + Int(length) - 1])
+  return buff
+}
+
 public func readZkey(filePath: String, fileType: String, maxVersion: Int) -> [String: Any] {
   let fileURL = URL(fileURLWithPath: filePath)
   do {
@@ -61,64 +75,90 @@ public func readZkey(filePath: String, fileType: String, maxVersion: Int) -> [St
     let protocolId = readULE32(data: bytesData, startPos: pointer)
     pointer += BIT_32
 
-    var zkey = [String: Any]()
+    var header = [String: Any]()
     if protocolId == 1 {
-      zkey["protocol"] = "groth16"
+      header["protocol"] = "groth16"
 
       pointer = sections[2][0].p
       let n8q = readULE32(data: bytesData, startPos: pointer)
-      zkey["n8q"] = n8q
+      header["n8q"] = n8q
       pointer += BIT_32
 
       let q = readBytes(data: bytesData, startPos: pointer, length: BIT_256)
-      zkey["q"] = q
+      header["q"] = q
       pointer += BIT_256
 
       let n8r = readULE32(data: bytesData, startPos: pointer)
-      zkey["n8r"] = n8r
+      header["n8r"] = n8r
       pointer += BIT_32
 
       let r = readBytes(data: bytesData, startPos: pointer, length: BIT_256)
-      zkey["r"] = r
+      header["r"] = r
       pointer += BIT_256
 
       // TODO: get curve
 
       let nVars = readULE32(data: bytesData, startPos: pointer)
-      zkey["nVars"] = nVars
+      header["nVars"] = nVars
       pointer += BIT_32
 
       let nPublic = readULE32(data: bytesData, startPos: pointer)
-      zkey["nPublic"] = nPublic
+      header["nPublic"] = nPublic
       pointer += BIT_32
 
       let domainSize = readULE32(data: bytesData, startPos: pointer)
-      zkey["domainSize"] = domainSize
+      header["domainSize"] = domainSize
       pointer += BIT_32
 
       let power = log2(Double(domainSize))
-      zkey["power"] = Int(power)
+      header["power"] = Int(power)
 
-      zkey["vk_alpha_1"] = readBytes(data: bytesData, startPos: pointer, length: BIT_512)
+      header["vk_alpha_1"] = readBytes(data: bytesData, startPos: pointer, length: BIT_512)
       pointer += BIT_512
 
-      zkey["vk_beta_1"] = readBytes(data: bytesData, startPos: pointer, length: BIT_512)
+      header["vk_beta_1"] = readBytes(data: bytesData, startPos: pointer, length: BIT_512)
       pointer += BIT_512
 
-      zkey["vk_beta_2"] = readBytes(data: bytesData, startPos: pointer, length: BIT_1024)
+      header["vk_beta_2"] = readBytes(data: bytesData, startPos: pointer, length: BIT_1024)
       pointer += BIT_1024
 
-      zkey["vk_gamma_2"] = readBytes(data: bytesData, startPos: pointer, length: BIT_1024)
+      header["vk_gamma_2"] = readBytes(data: bytesData, startPos: pointer, length: BIT_1024)
       pointer += BIT_1024
 
-      zkey["vk_delta_1"] = readBytes(data: bytesData, startPos: pointer, length: BIT_512)
+      header["vk_delta_1"] = readBytes(data: bytesData, startPos: pointer, length: BIT_512)
       pointer += BIT_512
 
-      zkey["vk_delta_2"] = readBytes(data: bytesData, startPos: pointer, length: BIT_1024)
+      header["vk_delta_2"] = readBytes(data: bytesData, startPos: pointer, length: BIT_1024)
       pointer += BIT_1024
     }
 
-    return zkey
+    // TODO: fix length
+    let ic = try readSection(data: bytesData, sections: sections, idSection: 3)
+    let buffCoeffs = try readSection(data: bytesData, sections: sections, idSection: 4)
+    let buffBasesA = try readSection(data: bytesData, sections: sections, idSection: 5)
+    let buffBasesB1 = try readSection(data: bytesData, sections: sections, idSection: 6)
+    let buffBasesB2 = try readSection(data: bytesData, sections: sections, idSection: 7)
+    let buffBasesC = try readSection(data: bytesData, sections: sections, idSection: 8)
+    let buffBasesH = try readSection(data: bytesData, sections: sections, idSection: 9)
+
+    var vk = [String: Any]()
+    vk["alpha_g1"] = header["vk_alpha_1"]
+    vk["beta_g2"] = header["vk_beta_2"]
+    vk["gamma_g2"] = header["vk_gamma_2"]
+    vk["delta_g2"] = header["vk_delta_2"]
+    vk["gamma_abc_g1"] = ic
+
+    var pk = [String: Any]()
+    pk["vk"] = vk
+    pk["beta_g1"] = header["vk_beta_1"]
+    pk["delta_g1"] = header["vk_delta_1"]
+    pk["a_query"] = buffBasesA
+    pk["b_g1_query"] = buffBasesB1
+    pk["b_g2_query"] = buffBasesB2
+    pk["l_query"] = buffBasesC
+    pk["h_query"] = buffBasesH
+
+    return pk
   } catch let error {
     print("Error: \(error)")
   }
--- Tests/snark-swiftTests/snark_swiftTests.swift
+++ Tests/snark-swiftTests/snark_swiftTests.swift
@@ -3,11 +3,11 @@ import XCTest
 @testable import snark_swift
 
 final class snark_swiftTests: XCTestCase {
-  func testExample() throws {
+  func testMultiplier2() throws {
     let fileType = "zkey"
     let filePath =
       "Tests/zkey/multiplier2_final.zkey"  // Replace with the actual file path
-    let zkey = readZkey(filePath: filePath, fileType: fileType, maxVersion: 2)
+    let pk = readZkey(filePath: filePath, fileType: fileType, maxVersion: 2)
     let vk_alpha_1: [UInt8] = [
       23, 14, 17, 42, 185, 164, 205, 1, 195, 107, 171, 71,
       64, 46, 252, 207, 233, 238, 75, 26, 225, 17, 222, 60,
@@ -32,8 +32,54 @@ final class snark_swiftTests: XCTestCase {
       161, 141, 247, 9, 98, 34, 251, 38, 245, 76, 14,
       93, 177, 216, 70, 21, 174, 34, 150, 9,
     ]
-    XCTAssertEqual(zkey["vk_alpha_1"] as! [UInt8], vk_alpha_1)
-    XCTAssertEqual(zkey["vk_beta_1"] as! [UInt8], vk_beta_1)
-    XCTAssertEqual(zkey["vk_delta_1"] as! [UInt8], vk_delta_1)
+    let vk: Any = pk["vk"]!
+    let vkDict = vk as! [String: Any]
+    let alpha_g1 = vkDict["alpha_g1"] as! [UInt8]
+    let beta_g1 = pk["beta_g1"]!
+    let delta_g1 = pk["delta_g1"]!
+
+    XCTAssertEqual(alpha_g1, vk_alpha_1)
+    XCTAssertEqual(beta_g1 as! [UInt8], vk_beta_1)
+    XCTAssertEqual(delta_g1 as! [UInt8], vk_delta_1)
+  }
+
+  func testKeccak() throws {
+    let fileType = "zkey"
+    let filePath =
+      "Tests/zkey/keccak256_256_test_final.zkey"  // Replace with the actual file path
+    let pk = readZkey(filePath: filePath, fileType: fileType, maxVersion: 2)
+    let vk_alpha_1: [UInt8] = [
+      23, 14, 17, 42, 185, 164, 205, 1, 195, 107, 171, 71,
+      64, 46, 252, 207, 233, 238, 75, 26, 225, 17, 222, 60,
+      207, 94, 92, 15, 152, 2, 235, 6, 30, 139, 14, 214,
+      223, 44, 75, 49, 54, 176, 41, 90, 23, 66, 228, 60,
+      120, 2, 126, 203, 170, 53, 127, 17, 146, 101, 59, 78,
+      218, 81, 70, 6,
+    ]
+    let vk_beta_1: [UInt8] = [
+      152, 64, 249, 69, 208, 4, 30, 157, 106, 160, 58,
+      151, 154, 79, 204, 163, 133, 120, 197, 150, 223, 183,
+      154, 46, 54, 212, 93, 207, 131, 241, 44, 24, 42,
+      162, 90, 167, 62, 183, 252, 205, 159, 181, 21, 6,
+      70, 106, 242, 84, 113, 200, 6, 37, 49, 124, 107,
+      225, 133, 195, 91, 134, 238, 176, 19, 4,
+    ]
+    let vk_delta_1: [UInt8] = [
+      180, 47, 63, 134, 88, 127, 113, 84, 108, 99, 138,
+      62, 34, 249, 239, 33, 25, 170, 36, 122, 66, 207,
+      30, 178, 105, 26, 25, 64, 190, 50, 67, 45, 254,
+      72, 18, 18, 187, 52, 69, 96, 171, 105, 104, 100,
+      252, 206, 225, 113, 51, 191, 213, 173, 48, 6, 161,
+      38, 202, 125, 193, 46, 119, 33, 142, 35,
+    ]
+    let vk: Any = pk["vk"]!
+    let vkDict = vk as! [String: Any]
+    let alpha_g1 = vkDict["alpha_g1"] as! [UInt8]
+    let beta_g1 = pk["beta_g1"]!
+    let delta_g1 = pk["delta_g1"]!
+
+    XCTAssertEqual(alpha_g1, vk_alpha_1)
+    XCTAssertEqual(beta_g1 as! [UInt8], vk_beta_1)
+    XCTAssertEqual(delta_g1 as! [UInt8], vk_delta_1)
   }
 }
